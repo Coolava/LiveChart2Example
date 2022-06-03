9b1809bd2dc38b437896c2d0abee8255335baf4a
YS Ryu <45839935+Coolava@users.noreply.github.com>
2022-06-03 13:16:44 +0900

    Add files via upload

---
+++ WpfApp1/WpfApp1/ViewModel.cs
@@ -0,0 +1,19 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using LiveChartsCore;
+using LiveChartsCore.SkiaSharpView;
+
+
+namespace WpfApp1;
+
+[ObservableObject]
+public partial class ViewModel
+{
+    public ISeries[] Series { get; set; } =
+    {
+        new LineSeries<double>
+        {
+            Values = new double[] { 2, 1, 3, 5, 3, 4, 6 },
+            Fill = null
+        }
+    };
+}
